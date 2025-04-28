5383d1bdd20f6612d9b630945058153f36d97bc0
ionut-gardu <ionut.gardu@emeal.nttdata.com>
2025-04-28 17:37:18 +0300

    Date filtering in home office  request dialog

--- src/app/components/home-office-request-dialog/home-office-request-dialog.component.ts
+++ src/app/components/home-office-request-dialog/home-office-request-dialog.component.ts
@@ -9,6 +9,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { HomeOfficeRequestEntry } from '../../models/home-office-request-entry.model';
+import { HomeOfficeRequestService } from '../../services/home-office-request.service';
 
 @Component({
   selector: 'app-home-office-request-dialog',
@@ -20,20 +22,40 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 export class HomeOfficeRequestDialogComponent {
   form: FormGroup;
   locations: HomeOfficeEntry[] = [];
-
+  requests : HomeOfficeRequestEntry[] = [];
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<HomeOfficeRequestDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private homeOfficeService: HomeOfficeService
+    private homeOfficeService: HomeOfficeService,
+    private homeOfficeRequestService: HomeOfficeRequestService
   ) {
     this.locations = this.homeOfficeService.getEntries();
+    this.requests = this.homeOfficeRequestService.getEntries();
     this.form = this.fb.group({
       address: ['', Validators.required],
       startDate: ['', Validators.required],
       endDate: ['', Validators.required],
     });
   }
+
+  dateFilter = (date: Date | null): boolean => {
+    if (!date) return false;
+    const day = date.getDay();
+    const isWeekend = day === 0 || day === 6; // 0 = Sunday, 6 = Saturday
+    if (isWeekend) return false;
+
+    // Check if the date falls within any existing request period
+    for (let request of this.requests) {
+      const requestStartDate = new Date(request.startDate);
+      const requestEndDate = new Date(request.endDate);
+      if (date >= requestStartDate && date <= requestEndDate) {
+        return false; // Disable this date if it overlaps with an existing request
+      }
+    }
+
+    return true;
+  };
 
   save() {
     if (this.form.valid) {
